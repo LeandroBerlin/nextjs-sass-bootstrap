d9c8e28b8094ea970118d027eabf59268521ff1e
Leandro <leandro.frigerio@devugees.org>
2019-09-12 21:16:02 +0200

    i wear my sunglasses at night

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Nextjs SCSS Bootstrap setup
+
+- Nextjs [quick setup](https://nextjs.org/learn/basics)
+- Next [Sass](https://github.com/zeit/next-plugins/tree/master/packages/next-sass)
+- Bootstrap
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import "../styles/index.scss"
+
+const Index = () => (
+    <div id="page-content">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-md-7">
+                    <h1 class="font-weight-light mt-4 text-white">Check your privileges</h1>
+                    <p class="lead text-white-50">Next.js SCSS Bootstrap</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Index;
