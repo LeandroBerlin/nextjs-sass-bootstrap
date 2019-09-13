a2c6736b3b281222591c7f62072091a6928a0ed1
Leandro <leandro.frigerio@devugees.org>
2019-09-13 21:20:01 +0200

    Smalltime boys

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import "../styles/index.scss"
 
 const Index = () => (
     <div id="page-content">
-        <div class="container text-center">
-            <div class="row justify-content-center">
-                <div class="col-md-7">
-                    <h1 class="font-weight-light mt-4 text-white">Check your privileges</h1>
-                    <p class="lead text-white-50">Next.js SCSS Bootstrap</p>
+        <div className="container text-center">
+            <div className="row justify-content-center">
+                <div className="col-md-7">
+                    <h1 className="font-weight-light mt-4 text-white">Check your privileges</h1>
+                    <p className="lead text-white-50">Next.js SCSS Bootstrap</p>
+                    <button type="button" className="btn btn-primary">Primary</button>
                 </div>
             </div>
         </div>
