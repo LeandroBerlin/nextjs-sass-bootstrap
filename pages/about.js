df11f21805f45772020d8454b6c1457d70f9007d
Leandro <leandro.frigerio@devugees.org>
2019-09-14 00:50:49 +0200

    dancing with tears in my eyes

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import "../styles/index.scss"
 
-const Index = () => (
+const About = () => (
     <div className="container text-center">
         <div className="row justify-content-center">
             <div className="col-md-7">
@@ -14,4 +14,4 @@ const Index = () => (
     </div >
 );
 
-export default Index;
+export default About;
